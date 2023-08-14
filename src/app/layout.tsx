5788b4faf87a7ea304b755443a79daa795c1bf3a
webdev3 <webdev3@qcgtech.com>
2023-08-15 00:48:57 +0700

    add css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,7 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs';
+import { ClerkProvider, currentUser } from '@clerk/nextjs';
 import { Metadata } from 'next';
 import React from 'react';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'DRY Project',
@@ -20,10 +15,34 @@ type Props = {
   children: React.ReactNode;
 };
 
+async function Header() {
+  const user = await currentUser();
+  if (!user)
+    return (
+      <>
+        <div className='navbar'>
+          <div className='frame'>
+            <Link href='/sign-in' className='custom-btn btn-1'>
+              Sign In
+            </Link>
+          </div>
+          <div className='frame'>
+            <Link href='/sign-up' className='custom-btn btn-1'>
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </>
+    );
+}
+
 export default function RootLayout({ children }: Props) {
   return (
     <ClerkProvider>
-      <html lang='en'>{children}</html>
+      <html lang='en'>
+        <Header />
+        {children}
+      </html>
     </ClerkProvider>
   );
 }
