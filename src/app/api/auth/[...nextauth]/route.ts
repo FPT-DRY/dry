6efd0ec7fca49b72a0fc7a09f42ef2be02f63198
ChienTan Kieu (Ryan) <kennyjoe131197@gmail.com>
2023-08-25 23:34:42 +0700

    fixed type issue on nextauth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,17 @@
+import authOptions from '@lib/auth';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth from 'next-auth';
 import { getToken } from 'next-auth/jwt';
-import authOptions from '@lib/auth';
+import { NextRequest, NextResponse } from 'next/server';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+type CustomNextRequest = NextApiRequest & NextRequest;
+type CustomNextResponse = NextApiResponse & NextResponse;
+
+const handler = async (req: CustomNextRequest, res: CustomNextResponse) => {
   const _token = getToken({ req, cookieName: 'dry-jwt-auth' });
   // Do whatever you want here, before the request is passed down to `NextAuth`
   return await NextAuth(req, res, authOptions);
 };
 
-export { handler as GET, handler as POST }; // FIXME: figure out why we can't build project due to type error on this line
+export { handler as GET, handler as POST };
+
